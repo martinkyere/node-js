4c1f7b2c5fb919e4503a3e4e4e52de501b2ee50d
martin kyere <martinkyere43@gmail.com>
2021-09-08 22:31:32 +0000

    initial commit

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
-// npm - global command, comes with node 
-// npm --version
+// EVENT LOOPS IN NODE JS
 
-// local dependency - use it only in this particular project
-// npm i <packageName>
+const { readFile } = require('fs')
 
-// global dependency - use it in any project
-// npm install -g <packageName>
-// sudo npm install -g <packageName> (windows)
-
-// package.jsom - manifest file (stores important info about project/package)
-// manual approach (create package.json in the root, ceate properties etc)
-// npm init (step by step, press enter to skip)
-// npm init -y (everything default)
-
-
-const _ = require('lodash')
-
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems)
-console.log('hello people')
-console.log('hello')
+console.log('start a first task')
+// CHECK FILE PATH
+readFile('./content/async.txt', (err, result) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    console.log(result)
+    console.log('complete next task')
+})
+console.log('starting next task')
