84364672a2ee4c004ed1782ed4fa2056b867ddb1
martin kyere <martinkyere43@gmail.com>
2021-09-08 23:06:13 +0000

    initial commit

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 // EVENT LOOPS IN NODE JS
 
-const { readFile } = require('fs')
+// const { readFile } = require('fs')
 
-console.log('start a first task')
-// CHECK FILE PATH
-readFile('./content/async.txt', (err, result) => {
-    if (err) {
-        console.log(err)
-        return
-    }
-    console.log(result)
-    console.log('complete next task')
-})
-console.log('starting next task')
+// console.log('start a first task')
+// // CHECK FILE PATH
+// readFile('./content/async.txt', 'utf8', (err, result) => {
+//     if (err) {
+//         console.log(err)
+//         return
+//     }
+//     console.log(result)
+//     console.log('complete next task')
+// })
+// console.log('starting next task')
+
+
+// SETTIMEOUT
+// started operating system process
+// console.log('first')
+// setTimeout(() => {
+//     console.log('second')
+// }, 3000)
+// console.log('third')
+// completed and exited operating system process
+
+
+
+// SETINTERVAL 
+// process stays alive unless
+// kill process CONTROL + C
+// unexpected error
+
+setInterval(() => {
+    console.log('hello world')
+}, 2000)
+console.log('I will run first')
